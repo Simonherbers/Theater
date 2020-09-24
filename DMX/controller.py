d8db354e0de54a00caa55f285b49bfd71559fa03
Simonherbers <55543874+Simonherbers@users.noreply.github.com>
2020-09-24 11:51:40 +0200

    Updated version of dmx-controller from Raspberry.

--- DMX/controller.py
+++ DMX/controller.py
@@ -1,6 +1,5 @@
 from pyftdi import ftdi
-from multiprocessing import Process, Queue
-import asyncio, time, threading
+import asyncio
 import paho.mqtt.client as mqtt
 
 #FTDI device info
@@ -15,8 +14,9 @@ topicTemplateCount = 9 # /Theater/
 class Device():
     def __init__(self, rawData):
         self.name = rawData
-        self.id = rawData[len(rawData) - 3: len(rawData) - 1]
+        self.id = int(rawData[len(rawData) - 2: len(rawData)])
         self.deviceType = rawData[0 : len(rawData) - 3]
+        self.topic = "/Theater/" + rawData
 
 ####################
 # DMX USB controller
@@ -29,21 +29,21 @@ class OpenDmxUsb():
         self.parity = 'N'
         self.flow_ctrl = ''
         self.rts_state = 0
-        #self._init_dmx()
+        self._init_dmx()
 
     #Initialize the controller
     def _init_dmx(self):
         self.ftdi=ftdi.Ftdi()
         self.ftdi.open(vendor,product,0)
         if self.ftdi.is_connected:
-            print("connected")
+            print("ftdi connected")
         else:
-            print("not connected")
+            print("ftdi not connected")
         self.ftdi.set_baudrate(self.baud_rate)
         self.ftdi.set_line_property(self.data_bits,
-                    self.stop_bits,
-                    self.parity,
-                    break_=0)
+                   self.stop_bits,
+                   self.parity,
+                   break_=0)
         self.ftdi.set_flowctrl(self.flow_ctrl)
         self.ftdi.purge_rx_buffer()
         self.ftdi.purge_tx_buffer()
@@ -51,7 +51,13 @@ class OpenDmxUsb():
 
     #Send DMX data
     def send_dmx(self,channelVals):
-        #Need to generate two bits for break
+        #Need to generate two bits for
+        dmx.ftdi.purge_rx_buffer()
+        dmx.ftdi.purge_tx_buffer() 
+        self.ftdi.set_line_property(self.data_bits,
+                   self.stop_bits,
+                   self.parity,
+                   break_=1)
         self.ftdi.set_line_property(self.data_bits,
                     self.stop_bits,
                     self.parity,
@@ -59,13 +65,9 @@ class OpenDmxUsb():
         self.ftdi.set_line_property(self.data_bits,
                     self.stop_bits,
                     self.parity,
-                    break_=1)
-        self.ftdi.set_line_property(self.data_bits,
-                    self.stop_bits,
-                    self.parity,
-                    break_=0)   
+                    break_=0)
         self.ftdi.write_data(channelVals)
-
+          
 
 
 dmx = OpenDmxUsb()
@@ -73,53 +75,25 @@ dmx = OpenDmxUsb()
 ########################
 # Read available Devices
 ########################
-
-path = "..\Interface\TheaterControl.Interface\Configuration\Devices.txt"
+path = "Devices.txt"
+#path = "..\Interface\TheaterControl.Interface\Configuration\Devices.txt"
 file = open(path, "r")
 lines = file.readlines()
 devices = []
 for line in lines:
-    devices.append(Device(line.strip()))
-
-print(len(devices))
+    devices.append(Device(line.strip().lower()))
 
 # Initial data
 
 #channel 0  1  2
+#Der erste muss 0 sein
 rList = [0, 0, 0]
 channel_vals = bytearray(rList)
 for x in bytearray([0]*510):
     channel_vals.append(0)
 
 # Queue to change the data sent to the adapter
-valueChangesQueue = Queue()
-
-#############################################
-# Process to permanently send data to adapter
-#############################################
-def coro(dmx) :
-    global valueChangesQueue
-    while True :
-        write(dmx)
-        if valueChangesQueue.qsize() > 0:
-            item = valueChangesQueue.get()
-            channel_vals[item[0]] = item[1]
-        
-        
-def write(dmx):
-    global channel_vals
-    # dmx.ftdi.set_break(True)
-    # dmx.ftdi.set_break(False)
-
-    #temporarily commented
-    # dmx.ftdi.purge_rx_buffer()
-    # dmx.ftdi.purge_tx_buffer()
-    # dmx.send_dmx(channel_vals)
-
-if __name__ == '__main__':    
-    p2 = Process(target=coro, args=(dmx,))
-    p2.start()
-
+valueChangesQueue = []
 
 
 ###################
@@ -130,31 +104,45 @@ def on_connect(client, userdata, flags, rc):
     global devices
     print("mqtt connected")
     for device in devices:
-        print(device.name)
-        client.subscribe(device.name)
+        client.subscribe(device.topic)
+        print(device.topic)
     
 def on_message(client, userdata, msg):
     global valueChangesQueue
     global devices
-    found = next(device for device in devices if device.name == msg.topic[topicTemplateCount - 1 : len(msg.topic)])
+    found = next(device for device in devices if device.topic == msg.topic)
     if found is not None:
-        valueChangesQueue.put([int(found.id),int(str(msg.payload))])
-        print(found.name + str(msg.payload))
+        value= int(msg.payload.decode("utf-8"))
+        if value > 100:
+            value = 100
+        elif value < 0:
+            value = 0
+        value = int(value * 255 / 100)
+        valueChangesQueue.append([found.id,value])
 
-if __name__ == '__main__':    
-    p2 = Process(target=coro, args=(dmx,))
-    p2.start()
+
 client = mqtt.Client(transport="websockets")
 
 client.on_message = on_message
 client.on_connect = on_connect
-client.on_disconnect = print("disconnected")
+client.on_disconnect = print("mqtt disconnected")
+#Kabel
+#client.connect("169.254.143.232", 9001, 60)
+#client.connect("192.168.178.52", 9001, 60)
+#client.connect("192.168.43.1", 9001, 60)
+client.connect("192.168.51.1", 9001, 60)
+client.loop_start()
 
-client.connect("169.254.143.232", 9001, 60)
-
-client.loop_forever()
-# print("wait")
-# time.sleep(5)
-
-# p2.terminate()
-# print("done")
+def main():
+    while True :
+        global valueChangesQueue 
+        item = []
+        if len(valueChangesQueue) > 0:
+            item = valueChangesQueue.pop()
+        if not item == []:
+            print("Item:")
+            print(item)
+            channel_vals[item[0]] = item[1]
+        dmx.send_dmx(channel_vals)
+    
+asyncio.ensure_future(main())
